414aa835e50cdc41f3fd6381976249ece7e6813d
Daniil Smirnov <danslapman@gmail.com>
2014-06-02 13:22:46 +0400

    GetBucketList implementation

--- src/Api/Buckets/BucketInfoProvider.cs
+++ src/Api/Buckets/BucketInfoProvider.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Api.Filesystem;
 
 namespace Api.Buckets
 {
     public class BucketInfoProvider : IBucketInfoProvider
     {
+        private readonly IFilesystemProvider _fsProvider;
+
+        public BucketInfoProvider(IFilesystemProvider fsProvider)
+        {
+            _fsProvider = fsProvider;
+        }
+
         public IList<BucketInfo> GetBucketList()
         {
-            throw new NotImplementedException();
+            return _fsProvider.ListRootDirectory().Select(s => new BucketInfo(s, DateTime.Now)).ToList();
         }
     }
 }
