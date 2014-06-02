e7946a0227eaa291d1a935c5b038db3b624a6df4
Daniil Smirnov <danslapman@gmail.com>
2014-06-02 14:34:04 +0400

    made BucketInfo XmlSerializator-compatible

--- src/Api/Buckets/BucketInfo.cs
+++ src/Api/Buckets/BucketInfo.cs
@@ -4,13 +4,7 @@ namespace Api.Buckets
 {
     public class BucketInfo
     {
-        public string Name { get; private set; }
-        public DateTime CreationDate { get; private set; }
-
-        public BucketInfo(string name, DateTime creationDate)
-        {
-            Name = name;
-            CreationDate = creationDate;
-        }
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
