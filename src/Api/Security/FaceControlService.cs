af9734899f09ae2bfa4b8602923295b7a4efb7cb
Daniil Smirnov <danslapman@gmail.com>
2014-06-09 18:43:58 +0400

    Incomplete CheckAuth implementation

--- src/Api/Security/FaceControlService.cs
+++ src/Api/Security/FaceControlService.cs
@@ -1,13 +1,44 @@
-﻿using Api.Domain;
+﻿using System.Linq;
+using Api.Domain;
+using LeviySoft.Extensions;
 using Nancy;
 
 namespace Api.Security
 {
     public class FaceControlService : IFaceControlService
     {
+        private BucketLordTemple _lordTemple;
+
+        public FaceControlService(BucketLordTemple lordTemple)
+        {
+            _lordTemple = lordTemple;
+        }
+
         public BucketLord CheckAuth(Request req)
         {
-            throw new System.NotImplementedException();
+            // TODO: implement querystring-based authentication
+            var authString = req.Headers.Authorization;
+            var authStringComponents = authString.Split(',');
+
+            if (authStringComponents.Count() != 4) return null;
+
+            var authParams =
+                authStringComponents.Where(c => c.Contains("="))
+                    .Select(c => c.Split('='))
+                    .ToDictionary(c => c[0], c => c[1]);
+
+            if (authParams.ContainsKeys("Credential", "SignedHeaders", "Signature"))
+            {
+                var credentials = authParams["Credential"].Split('/');
+                var accessKeyId = credentials[0];
+
+                var bucketLord = _lordTemple.FindLordByAccessKeyId(accessKeyId);
+                if (bucketLord == null) return null;
+
+                //TODO: complete
+            }
+
+            return null;
         }
     }
 }
