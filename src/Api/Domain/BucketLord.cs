59d0d12b6d4aa6c4f69c163879b12b48d7100bf9
Daniil Smirnov <danslapman@gmail.com>
2014-06-09 17:28:07 +0400

    using BucketLord as IUserIdentity for Nancy

--- src/Api/Domain/BucketLord.cs
+++ src/Api/Domain/BucketLord.cs
@@ -1,10 +1,16 @@
-﻿namespace Api.Domain
+﻿using System.Collections.Generic;
+using Nancy.Security;
+
+namespace Api.Domain
 {
-    public class BucketLord
+    public class BucketLord : IUserIdentity
     {
         public string ID { get; set; }
         public string DisplayName { get; set; }
         public string AccessKeyID { get; set; }
         public string SecretKey { get; set; }
+
+        public string UserName { get { return DisplayName; } }
+        public IEnumerable<string> Claims { get { return new List<string>(); } } 
     }
 }
