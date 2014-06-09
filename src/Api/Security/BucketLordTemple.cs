d3294fb657c32ff0c6c4466f616b8336348cf3d8
Daniil Smirnov <danslapman@gmail.com>
2014-06-09 18:06:31 +0400

    Basic BucketLordTemple implementation

---
+++ src/Api/Security/BucketLordTemple.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using Api.Domain;
+
+namespace Api.Security
+{
+    public class BucketLordTemple : IBucketLordTemple
+    {
+        private readonly IList<BucketLord> _lords; 
+        private readonly XmlSerializer _bucketLordListSerializer = new XmlSerializer(typeof (List<BucketLord>));
+
+        public BucketLordTemple()
+        {
+            _lords = new List<BucketLord>();
+            _lords =
+                _bucketLordListSerializer.Deserialize(File.Open("bucketlords.xml", FileMode.Open)) as List<BucketLord>;
+        }
+
+        public BucketLord CallBucketLord(string name)
+        {
+            return _lords.SingleOrDefault(l => l.UserName == name);
+        }
+
+        public BucketLord FindLordByAccessKeyId(string accessKeyId)
+        {
+            return _lords.SingleOrDefault(l => l.AccessKeyID == accessKeyId);
+        }
+    }
+}
