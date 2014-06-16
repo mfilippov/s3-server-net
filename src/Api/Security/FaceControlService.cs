8872cbd69521505e4dccefc76628713bcc4ea804
Daniil Smirnov <danslapman@gmail.com>
2014-06-16 15:17:55 +0400

    S3Request .Validate() implementation

--- src/Api/Security/FaceControlService.cs
+++ src/Api/Security/FaceControlService.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Api.Domain;
-using LeviySoft.Extensions;
+﻿using Api.Domain;
 using Nancy;
-using Nancy.Extensions;
 
 namespace Api.Security
 {
@@ -18,31 +14,14 @@ namespace Api.Security
 
         public BucketLord CheckAuth(Request req)
         {
-            // TODO: implement querystring-based authentication
-            var credentials = S3AuthorizationHeader.ParseHeader(req.Headers.Authorization);
+            var s3Req = req.ToS3Request();
 
-            if (credentials.Credentials.AccessKeyId.IsNotEmpty())
+            if (s3Req != null)
             {
-                var bucketLord = _lordTemple.FindLordByAccessKeyId(credentials.Credentials.AccessKeyId);
+                var bucketLord = _lordTemple.FindLordByAccessKeyId(s3Req.S3Authorization.Credentials.AccessKeyId);
                 if (bucketLord == null) return null;
-                //date: {5/24/2013 12:00:00 AM}
-                //secret: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-                //region: us-east-1
-                //path: "/"
-                //TODO: Fix it, this code have problem with double signing with Authorization header.
-                /*var signature = S3F.ComputeSignature(credentials.Credentials.Date, bucketLord.SecretKey,
-                    credentials.Credentials.Region,
-                    S3F.CreateStringToSign(credentials.Credentials.Date, credentials.Credentials.Region,
-                        S3F.CreateCanonicalRequest(req.Method, req.Path, string.Empty,
-                            new SortedDictionary<string, string>(req.Headers.ToDictionary(p => p.Key, p => string.Join(",", p.Value))),
-                            credentials.SignedHeaders.ToList(), req.Body.AsString())));*/
 
-                var stringForSign = S3F.CreateStringToSign(credentials.Credentials.Date, credentials.Credentials.Region,
-                    S3F.CreateCanonicalRequest(req.Method, req.Path, string.Empty, new SortedDictionary<string, string>(), credentials.SignedHeaders.ToList(), ""));
-
-                var signature = S3F.ComputeSignature(credentials.Credentials.Date, bucketLord.SecretKey,
-                    credentials.Credentials.Region, stringForSign);
-                if (signature == credentials.Signature) return bucketLord;
+                if (s3Req.Validate(bucketLord)) return bucketLord;
             }
 
             return null;
